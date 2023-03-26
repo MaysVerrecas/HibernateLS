91f9e3cf76e646e922f85f183f7bce230386fe97
MaysVerrecas <99118212+MaysVerrecas@users.noreply.github.com>
2023-03-26 23:01:09 +0300

    done

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -13,10 +13,23 @@ public class Application {
         EmployeeDao employeeDao = new EmployeeDaoImpl();
         CityDao cityDao = new CityDaoImpl();
 
+        /*Employee employee = new Employee("Makar", "Makarevich", "m", 21, 1);
+        employeeDao.createEmployee(employee);
+        System.out.println(employeeDao.findById(1));
+        List<Employee> list = employeeDao.findAll();
+        list.stream()
+                .forEach(System.out::println);
+        Employee employee1 = employeeDao.findById(employee.getId());
+        employee1.setAge(234234234);
+        employeeDao.updateEmployee(employee);
+        employeeDao.delete(employee);*/
 
+        //City city = new City("Istanbul");
+        //cityDao.createCity(city);
         Employee employee = new Employee("Petr", "Petrov", "M", 34, cityDao.findById(1));
         employeeDao.createEmployee(employee);
         System.out.println(employeeDao.findAll());
         System.out.println(cityDao.findAll());
+        //System.out.println(cityDao.findAllEmployeeByCityId(1));
     }
 }
